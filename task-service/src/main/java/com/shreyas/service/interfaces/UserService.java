5837cf4b738d2fd4abdb5fc4ae32be8a0f174cd5
Shreyas Kothari <spkothari0@gmail.com>
2024-09-08 21:12:32 -0400

    Adding Resilience4j to the Task-service

--- task-service/src/main/java/com/shreyas/service/interfaces/UserService.java
+++ task-service/src/main/java/com/shreyas/service/interfaces/UserService.java
@@ -1,12 +1,20 @@
 package com.shreyas.service.interfaces;
 
 import com.shreyas.config.FeignClientConfig;
+import com.shreyas.controller.APIResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "USER-SERVICE", configuration = FeignClientConfig.class)
 public interface UserService {
     @GetMapping("/api/v1/user/")
+    @CircuitBreaker(name = "user", fallbackMethod = "getUserFallback")
     Object getUser(@RequestHeader("Authorization") String token);
+
+    default Object getUserFallback(String token, Throwable t) {
+        return new APIResponse<Void>("ERROR", null, "User Service unavailable at the moment. Please try later", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
